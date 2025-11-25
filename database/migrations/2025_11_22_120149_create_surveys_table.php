db1f1c6ea607c5b971a44b4c91214c4eb3e6c969
arcadiumoff <arcadium.off@gmail.com>
2025-11-25 10:50:12 +0100

    base config and add user

--- database/migrations/2025_11_22_120149_create_surveys_table.php
+++ database/migrations/2025_11_22_120149_create_surveys_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->unsignedBigInteger('organization_id');
             $table->unsignedBigInteger('user_id');
             $table->string('title');
+            $table->string('token')->unique();
             $table->text('description');
             $table->dateTime('start_date');
             $table->dateTime('end_date');
