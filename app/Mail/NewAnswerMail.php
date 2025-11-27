60cde80f2796cb29a17806881e01aa3217c2ceaf
Liam <liam.deparfouru@edu.esiee-it.fr>
2025-11-27 22:34:01 +0100

    ADD | NewAnswerMail please

--- app/Mail/NewAnswerMail.php
+++ app/Mail/NewAnswerMail.php
@@ -37,7 +37,7 @@ class NewAnswerMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'emails.new_answer_mail',
         );
     }
 
